9b101bb9e4a457bc45469c49236a0a9ebc7b13e4
Davies, Mark <mark.davies@camdenai.com>
2020-12-28 17:25:01 +0000

    adding repo interfaces and service interfaces

---
+++ Job_Bookings.Service/Repos/Interfaces/IUserPreferencesRepo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Job_Bookings.Services.Repos.Interfaces
+{
+    public interface IUserPreferencesRepo
+    {
+    }
+}
